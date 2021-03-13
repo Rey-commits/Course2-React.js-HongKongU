1c12e52f1a6e4c491d107de094d919e7b7698a14
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-03-13 13:58:36 +0800

    Forms have been added

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -63,6 +63,47 @@ class Contact extends Component {
                         </div>
                     </div>
                 </div>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Send us Your Feedback</h3>
+                    </div>
+                    <div className="col-12 col-md-9">
+                        <Form>
+                            <FormGroup row>
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstname" name="firstname"
+                                           placeholder="First Name" 
+                                           value={this.state.firstname}/>                                    
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastname" name="lastname"
+                                           placeholder="Last Name" 
+                                           value={this.state.lastname}/>    
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="telnum" md={2}>Contact</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="telnum" name="telnum"
+                                           placeholder="Tel. Number" 
+                                           value={this.state.telnum}/>                                    
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                           placeholder="Email" 
+                                           value={this.state.firstname}/>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </div>
+                </div>
             </div>
         );
     }
